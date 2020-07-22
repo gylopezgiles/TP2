0a9ffb85153f9ec4ba712a51bd32f30a2ef743d8
Gisela Lopez Giles <gisela.lopez@mercadolibre.com>
2020-07-22 18:21:00 -0300

    remuevo el archivo iml

--- src/main/java/edu/fiuba/algo3/modelo/pregunta/CreadorPregunta.java
+++ src/main/java/edu/fiuba/algo3/modelo/pregunta/CreadorPregunta.java
@@ -5,14 +5,11 @@ import java.util.List;
 public class CreadorPregunta {
 
     public static Preguntable crearPregunta(TipoPregunta tipoPregunta, List<Opcion> opciones){
-
-        Preguntable preguntaACrear ;
         switch (tipoPregunta) {
-            case VerdaderoFalsoClasico: preguntaACrear = new VerdaderoFalsoClasico(opciones);
-            default: preguntaACrear = new VerdaderoFalsoClasico(opciones);
+            case VerdaderoFalsoClasico:
+                return new VerdaderoFalsoClasico(opciones);
+            default:
+                return null;
         }
-
-
-        return preguntaACrear;
     }
 }
