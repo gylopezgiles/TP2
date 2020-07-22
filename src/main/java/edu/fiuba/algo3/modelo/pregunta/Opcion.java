bc63a236fb4286aa7590e0636df0b8fff61ad7c7
Micaela Villordo <mvillordo@fi.uba.ar>
2020-07-22 12:16:59 -0300

    agrego funcionalidad verdadero-falso asigna puntaje

--- src/main/java/edu/fiuba/algo3/modelo/pregunta/Opcion.java
+++ src/main/java/edu/fiuba/algo3/modelo/pregunta/Opcion.java
@@ -2,8 +2,12 @@ package edu.fiuba.algo3.modelo.pregunta;
 
 public class Opcion {
 
+    private String texto;
+    private Boolean correcta;
+
     public Opcion(String texto, Boolean correcta){
-        //TODO
+        this.texto = texto;
+        this.correcta = correcta;
     }
 
     public Boolean esCorrecta(){
--- src/main/java/edu/fiuba/algo3/modelo/pregunta/VerdaderoFalsoClasico.java
+++ src/main/java/edu/fiuba/algo3/modelo/pregunta/VerdaderoFalsoClasico.java
@@ -4,19 +4,22 @@ import java.util.List;
 
 public class VerdaderoFalsoClasico implements Preguntable {
 
+    private List<Opcion> opciones;
+    private int puntaje;
+
     VerdaderoFalsoClasico(List<Opcion> opciones){
-        //TODO
+        this.opciones = opciones;
     }
 
     @Override
     public List<Opcion> obtenerOpciones() {
-        //TODO
-        return null;
+        return this.opciones;
     }
 
     @Override
     public int establecerPuntuacion(List<Opcion> opciones) {
-        //TODO
-        return 0;
+        if(opciones.get(0) == this.opciones.get(0))
+            this.puntaje = 1;
+        return this.puntaje;
     }
 }
