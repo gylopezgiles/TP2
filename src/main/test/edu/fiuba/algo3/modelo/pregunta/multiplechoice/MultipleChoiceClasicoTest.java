dba3cac1e211eaeb779b5a2c240c3cfd5f302ae3
Mario Besednjak <mbesednjak@fi.uba.ar>
2020-07-27 18:47:03 -0300

    Prueba MultipleChoiceClasico

---
+++ src/main/test/edu/fiuba/algo3/modelo/pregunta/multiplechoice/MultipleChoiceClasicoTest.java
@@ -0,0 +1,14 @@
+package edu.fiuba.algo3.modelo.pregunta.multiplechoice;
+
+import edu.fiuba.algo3.modelo.excepciones.ParametrosInvalidosExcepcion;
+import edu.fiuba.algo3.modelo.pregunta.Opcion;
+import edu.fiuba.algo3.modelo.pregunta.Preguntable;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MultipleChoiceClasicoTest {
+}
