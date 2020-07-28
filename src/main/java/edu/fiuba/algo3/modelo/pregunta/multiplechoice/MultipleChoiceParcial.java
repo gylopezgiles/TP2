ace0327bd6c980f4b96b6b18b5203fabd9bc400d
Micaela Villordo <mvillordo@fi.uba.ar>
2020-07-27 21:15:36 -0300

    agrego tests y funcionalidad para asignar puntaje en multiple chocie parcial

--- src/main/java/edu/fiuba/algo3/modelo/pregunta/multiplechoice/MultipleChoiceParcial.java
+++ src/main/java/edu/fiuba/algo3/modelo/pregunta/multiplechoice/MultipleChoiceParcial.java
@@ -4,6 +4,7 @@ import edu.fiuba.algo3.modelo.excepciones.ParametrosInvalidosExcepcion;
 import edu.fiuba.algo3.modelo.pregunta.Opcion;
 
 import java.util.List;
+import java.util.Optional;
 
 public class MultipleChoiceParcial extends MultipleChoice {
 
@@ -15,7 +16,18 @@ public class MultipleChoiceParcial extends MultipleChoice {
 
     @Override
     public int establecerPuntuacion(List<Opcion> opciones) {
-        return 0;
+        int puntaje = puntajeOpcionesCorrectas(opciones);
+        Optional<Opcion> opcion = opciones.stream()
+                .filter(op -> !op.esCorrecta())
+                .findAny();
+        return opcion.isPresent() ? 0 : puntaje;
+    }
+
+    private int puntajeOpcionesCorrectas(List<Opcion> opciones) {
+        long puntaje = opciones.stream()
+                .filter(op -> op.esCorrecta())
+                .count();
+        return (int)puntaje;
     }
 
 }
