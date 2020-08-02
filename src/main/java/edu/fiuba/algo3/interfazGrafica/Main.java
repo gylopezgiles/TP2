22f4e3826d9bc9efc283af42f0cc30ee018201c8
Luis <luisjosepr@gmail.com>
2020-08-02 12:36:28 -0300

    Se implemento botones y etiquetas
    Implementacion de botones y etiquetas

--- src/main/java/edu/fiuba/algo3/interfazGrafica/Main.java
+++ src/main/java/edu/fiuba/algo3/interfazGrafica/Main.java
@@ -1,7 +1,9 @@
-package edu.fiuba.algo3.modelo.interfazGrafica;
+package edu.fiuba.algo3.interfazGrafica;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -15,6 +17,16 @@ public class Main extends Application {
         stage.setTitle("Kahoot");
 
         StackPane layout = new StackPane();
+
+        Label label = new Label();
+        label.setText("Texto de etiqueta");
+        layout.getChildren().add(label);
+
+        Button boton = new Button();
+        boton.setText("Texto de boton");
+        layout.getChildren().add(boton);
+
+
         Scene scene = new Scene(layout);
         stage.setScene(scene);
 
