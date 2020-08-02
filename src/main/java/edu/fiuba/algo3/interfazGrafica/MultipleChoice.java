1bebe849362da9527bad9594f3c825a23d3d70da
Luis <luisjosepr@gmail.com>
2020-08-02 15:47:00 -0300

    Mock Up inicial del juego
    Diseño original de la interfaz grafica. Aun no tiene funcionamiento, solo estética.

--- src/main/java/edu/fiuba/algo3/interfazGrafica/MultipleChoice.java
+++ src/main/java/edu/fiuba/algo3/interfazGrafica/MultipleChoice.java
@@ -2,14 +2,29 @@ package edu.fiuba.algo3.interfazGrafica;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
+
+
+
 public class MultipleChoice extends Application {
+
+    private void centrar_alinear_fila(HBox fila, int alineamiento ){
+        fila.setAlignment(Pos.CENTER);
+        fila.setSpacing(alineamiento);
+    }
 
     public static void main(String[] args){
         launch(args);
@@ -19,37 +34,59 @@ public class MultipleChoice extends Application {
     public void start(Stage stage) throws Exception {
         stage.setTitle("Multiple Choice");
 
-        Label pregunta = new Label();
-        pregunta.setText("Elija una opcion entre 1 a 5");
+        //Encabezado
+        FileInputStream entrada = new FileInputStream("doc/imagenes/logo_kahoot.jpg");
+        Image imagen = new Image(entrada);
+        ImageView vistaKahoot = new ImageView(imagen);
+
+        Text bienvenida = new Text("Bienvenizo al juego del grupo N1 turno noche");
+        bienvenida.setFont(Font.font("Arial", FontWeight.BOLD, 11));
+
+        HBox encabezado = new HBox(vistaKahoot, bienvenida);
+        centrar_alinear_fila(encabezado, 20);
+
+
+        // Cuerpo
+        Text pregunta = new Text("¿2 + 2 = 4?");
+        pregunta.setFont(Font.font("Arial", FontWeight.NORMAL, 20));
 
         Button opcion1 = new Button();
-        opcion1.setText("Opcion 1");
+        opcion1.setText("opcion 1");
 
         Button opcion2 = new Button();
-        opcion2.setText("Opcion 2");
+        opcion2.setText("opcion 2");
 
         Button opcion3 = new Button();
-        opcion3.setText("Opcion 3");
+        opcion3.setText("opcion 3");
 
         Button opcion4 = new Button();
-        opcion4.setText("Opcion 4");
+        opcion4.setText("opcion 4");
 
         Button opcion5 = new Button();
-        opcion5.setText("Opcion 5");
+        opcion5.setText("opcion 5");
 
-        HBox fila1 = new HBox(opcion1, opcion2, opcion3);
-        fila1.setSpacing(10);
+        HBox fila1opciones = new HBox(opcion1, opcion2, opcion3);
+        centrar_alinear_fila(fila1opciones, 3);
 
-        HBox fila2 = new HBox(opcion4, opcion5);
-        fila2.setSpacing(10);
+        HBox fila2opciones = new HBox(opcion4, opcion5);
+        centrar_alinear_fila(fila2opciones, 3);
 
-        VBox contenedorPrincipal = new VBox(pregunta ,fila1, fila2);
-        contenedorPrincipal.setSpacing(10);
-        contenedorPrincipal.setPadding(new Insets(20));
+        VBox contenedorOpciones = new VBox(fila1opciones, fila2opciones);
+        contenedorOpciones.setSpacing(5);
 
-        Scene scene = new Scene(contenedorPrincipal, 300, 300);
+
+        VBox contenedorPrincipal = new VBox(pregunta, contenedorOpciones);
+        contenedorPrincipal.setSpacing(60);
+        contenedorPrincipal.setPadding(new Insets(40));
+        contenedorPrincipal.setAlignment(Pos.CENTER);
+
+        //Cuerpo del Programa
+        BorderPane cuerpo = new BorderPane();
+        cuerpo.setTop(encabezado);
+        cuerpo.setCenter(contenedorPrincipal);
+
+        Scene scene = new Scene(cuerpo, 400, 400);
         stage.setScene(scene);
-
         stage.show();
     }
 
--- src/main/java/edu/fiuba/algo3/interfazGrafica/VerdaderoFalso.java
+++ src/main/java/edu/fiuba/algo3/interfazGrafica/VerdaderoFalso.java
@@ -2,14 +2,29 @@ package edu.fiuba.algo3.interfazGrafica;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
+
 public class VerdaderoFalso extends Application {
+
+    private void centrar_alinear_fila(HBox fila, int alineamiento ){
+        fila.setAlignment(Pos.CENTER);
+        fila.setSpacing(alineamiento);
+    }
+
+
     public static void main(String[] args){
         launch(args);
     }
@@ -18,8 +33,23 @@ public class VerdaderoFalso extends Application {
     public void start(Stage stage) throws Exception {
         stage.setTitle("Verdadero o Falso");
 
-        Label pregunta = new Label();
-        pregunta.setText("¿2 + 2 = 4?");
+    //Encabezado
+        FileInputStream entrada = new FileInputStream("doc/imagenes/logo_kahoot.jpg");
+        Image imagen = new Image(entrada);
+        ImageView vistaKahoot = new ImageView(imagen);
+
+
+        Text bienvenida = new Text("Bienvenizo al juego del grupo N1 turno noche");
+        bienvenida.setFont(Font.font("Arial", FontWeight.BOLD, 11));
+
+        HBox encabezado = new HBox(vistaKahoot, bienvenida);
+        centrar_alinear_fila(encabezado, 20);
+
+
+    // Cuerpo
+        Text pregunta = new Text("¿2 + 2 = 4?");
+        pregunta.setFont(Font.font("Arial", FontWeight.NORMAL, 20));
+
 
         Button opcionVerdadera = new Button();
         opcionVerdadera.setText("True");
@@ -27,16 +57,22 @@ public class VerdaderoFalso extends Application {
         Button opcionFalsa = new Button();
         opcionFalsa.setText("False");
 
-        HBox contenedorHorizontal = new HBox(opcionVerdadera, opcionFalsa);
-        contenedorHorizontal.setSpacing(10);
+        HBox contenedorOpciones = new HBox(opcionVerdadera, opcionFalsa);
+        centrar_alinear_fila(contenedorOpciones, 10);
 
-        VBox contenedorPrincipal = new VBox(pregunta ,contenedorHorizontal);
-        contenedorPrincipal.setSpacing(10);
-        contenedorPrincipal.setPadding(new Insets(20));
 
-        Scene scene = new Scene(contenedorPrincipal, 300, 300);
+        VBox contenedorPrincipal = new VBox(pregunta, contenedorOpciones);
+        contenedorPrincipal.setSpacing(60);
+        contenedorPrincipal.setPadding(new Insets(40));
+        contenedorPrincipal.setAlignment(Pos.CENTER);
+
+    //Cuerpo del programa
+        BorderPane cuerpo = new BorderPane();
+        cuerpo.setTop(encabezado);
+        cuerpo.setCenter(contenedorPrincipal);
+
+        Scene scene = new Scene(cuerpo, 400, 400);
         stage.setScene(scene);
-
         stage.show();
     }
 }
