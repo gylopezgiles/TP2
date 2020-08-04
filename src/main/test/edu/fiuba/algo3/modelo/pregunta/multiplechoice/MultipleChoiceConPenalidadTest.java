71ad7fe2f308837215a8cc68b811027196c96ce7
Micaela Villordo <mvillordo@fi.uba.ar>
2020-08-04 12:30:24 -0300

    cambios para unificar convenciones en los tests

--- src/main/test/edu/fiuba/algo3/modelo/pregunta/multiplechoice/MultipleChoiceConPenalidadTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/pregunta/multiplechoice/MultipleChoiceConPenalidadTest.java
@@ -27,7 +27,7 @@ public class MultipleChoiceConPenalidadTest {
     @Test
     public void debeLanzarExcepcionCrearPreguntaConCantidadOpcionesMenorA2() {
 
-        boolean esCorrecta = true;
+        Boolean esCorrecta = Boolean.TRUE;
         Opcion opcion = new Opcion("Lima", esCorrecta);
         List<Opcion> opciones = Arrays.asList(opcion);
 
@@ -38,7 +38,7 @@ public class MultipleChoiceConPenalidadTest {
     @Test
     public void debeLanzarExcepcionCrearPreguntaConCantidadOpcionesMayorA5() {
 
-        boolean esCorrecta = true;
+        Boolean esCorrecta = Boolean.TRUE;
         Opcion opcion1 = new Opcion("Rusia", esCorrecta);
         Opcion opcion2 = new Opcion("Bolivia", !esCorrecta);
         Opcion opcion3 = new Opcion("Uruguay", !esCorrecta);
@@ -54,7 +54,7 @@ public class MultipleChoiceConPenalidadTest {
     @Test
     public void debeLanzarExcepcionCrearPreguntaSinOpcionesCorrectas() {
 
-        boolean esCorrecta = true;
+        Boolean esCorrecta = Boolean.TRUE;
         Opcion opcion1 = new Opcion("Francia", !esCorrecta);
         Opcion opcion2 = new Opcion("Australia", !esCorrecta);
         Opcion opcion3 = new Opcion("Bolivia", !esCorrecta);
@@ -68,13 +68,13 @@ public class MultipleChoiceConPenalidadTest {
 
     @Test
     public void debeEstablecerPuntuacionCorrectamenteEligiendoTodasRespuestasCorrectasSinIncorrectas() throws ParametrosInvalidosExcepcion {
-        boolean esCorrecta = true;
+        String preguntaTexto = "Qué países se encuentran en Asia?";
+        Boolean esCorrecta = Boolean.TRUE;
         Opcion opcion1Correcta = new Opcion("China", esCorrecta);
         Opcion opcion2Correcta = new Opcion("India", esCorrecta);
         Opcion opcion3Correcta = new Opcion("Japón", esCorrecta);
         Opcion opcion4Incorrecta = new Opcion("España", !esCorrecta);
         List<Opcion> opciones = Arrays.asList(opcion1Correcta, opcion2Correcta, opcion3Correcta, opcion4Incorrecta);
-        String preguntaTexto = "Qué países se encuentran en Asia?";
         Preguntable multipleChoiceConPenalidad = new MultipleChoiceConPenalidad(preguntaTexto, opciones);
         List<Opcion> opcionesSeleccionadas = Arrays.asList(opcion1Correcta, opcion2Correcta, opcion3Correcta);
 
@@ -85,13 +85,13 @@ public class MultipleChoiceConPenalidadTest {
 
     @Test
     public void debeEstablecerPuntuacionCorrectamenteEligiendoAlgunasRespuestasCorrectasSinIncorrectas() throws ParametrosInvalidosExcepcion {
-        boolean esCorrecta = true;
+        String preguntaTexto = "Qué países se encuentran en Europa?";
+        Boolean esCorrecta = Boolean.TRUE;
         Opcion opcion1Correcta = new Opcion("España", esCorrecta);
         Opcion opcion2Correcta = new Opcion("Francia", esCorrecta);
         Opcion opcion3Correcta = new Opcion("Alemania", esCorrecta);
         Opcion opcion4Incorrecta = new Opcion("Ecuador", !esCorrecta);
         List<Opcion> opciones = Arrays.asList(opcion1Correcta, opcion2Correcta, opcion3Correcta, opcion4Incorrecta);
-        String preguntaTexto = "Qué países se encuentran en Europa?";
         Preguntable multipleChoiceConPenalidad = new MultipleChoiceConPenalidad(preguntaTexto, opciones);
         List<Opcion> opcionesSeleccionadas = Arrays.asList(opcion1Correcta, opcion3Correcta);
 
@@ -102,13 +102,13 @@ public class MultipleChoiceConPenalidadTest {
 
     @Test
     public void debeEstablecerPuntuacionCorrectamenteEligiendoAlgunasRespuestasCorrectasYUnaIncorrecta() throws ParametrosInvalidosExcepcion {
-        boolean esCorrecta = true;
+        String preguntaTexto = "Qué países se encuentran en África?";
+        Boolean esCorrecta = Boolean.TRUE;
         Opcion opcion1Correcta = new Opcion("Egipto", esCorrecta);
         Opcion opcion2Correcta = new Opcion("Kenia", esCorrecta);
         Opcion opcion3Incorrecta = new Opcion("Brasil", !esCorrecta);
         Opcion opcion4Incorrecta = new Opcion("Estonia", !esCorrecta);
         List<Opcion> opciones = Arrays.asList(opcion1Correcta, opcion2Correcta, opcion3Incorrecta, opcion4Incorrecta);
-        String preguntaTexto = "Qué países se encuentran en África?";
         Preguntable multipleChoiceConPenalidad = new MultipleChoiceConPenalidad(preguntaTexto, opciones);
         List<Opcion> opcionesSeleccionadas = Arrays.asList(opcion1Correcta, opcion2Correcta, opcion3Incorrecta);
 
@@ -119,14 +119,14 @@ public class MultipleChoiceConPenalidadTest {
 
     @Test
     public void debeEstablecerPuntuacionCorrectamenteEligiendoAlgunasRespuestasCorrectasYAlgunasIncorrecta() throws ParametrosInvalidosExcepcion {
-        boolean esCorrecta = true;
+        String preguntaTexto = "Qué países se encuentran en América del Sur?";
+        Boolean esCorrecta = Boolean.TRUE;
         Opcion opcion1Correcta = new Opcion("Chile", esCorrecta);
         Opcion opcion2Correcta = new Opcion("Paraguay", esCorrecta);
         Opcion opcion3Incorrecta = new Opcion("México", !esCorrecta);
         Opcion opcion4Incorrecta = new Opcion("Panamá", !esCorrecta);
         Opcion opcion5Incorrecta = new Opcion("Estados Unidos", !esCorrecta);
         List<Opcion> opciones = Arrays.asList(opcion1Correcta, opcion2Correcta, opcion3Incorrecta, opcion4Incorrecta, opcion5Incorrecta);
-        String preguntaTexto = "Qué países se encuentran en América del Sur?";
         Preguntable multipleChoiceConPenalidad = new MultipleChoiceConPenalidad(preguntaTexto, opciones);
         List<Opcion> opcionesSeleccionadas = Arrays.asList(opcion1Correcta, opcion3Incorrecta, opcion4Incorrecta);
 
@@ -137,14 +137,14 @@ public class MultipleChoiceConPenalidadTest {
 
     @Test
     public void debeEstablecerPuntuacionCorrectamenteEligiendoTodasRespuestasIncorrectas() throws ParametrosInvalidosExcepcion {
-        boolean esCorrecta = true;
+        String preguntaTexto = "Qué países se encuentran en América Central?";
+        Boolean esCorrecta = Boolean.TRUE;
         Opcion opcion1Correcta = new Opcion("Guatemala", esCorrecta);
         Opcion opcion2Correcta = new Opcion("Cuba", esCorrecta);
         Opcion opcion3Incorrecta = new Opcion("Rusia", !esCorrecta);
         Opcion opcion4Incorrecta = new Opcion("Lituania", !esCorrecta);
         Opcion opcion5Incorrecta = new Opcion("Estados Unidos", !esCorrecta);
         List<Opcion> opciones = Arrays.asList(opcion1Correcta, opcion2Correcta, opcion3Incorrecta, opcion4Incorrecta, opcion5Incorrecta);
-        String preguntaTexto = "Qué países se encuentran en América Central?";
         Preguntable multipleChoiceConPenalidad = new MultipleChoiceConPenalidad(preguntaTexto, opciones);
         List<Opcion> opcionesSeleccionadas = Arrays.asList(opcion3Incorrecta, opcion4Incorrecta, opcion5Incorrecta);
 
