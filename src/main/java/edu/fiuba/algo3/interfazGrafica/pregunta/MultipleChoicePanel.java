6846470667fd2d7b1f446c5155ef0131c03a0389
besednjak <mario.besed@gmail.com>
2020-08-16 21:19:58 -0300

    Multiple choice panel completo

--- src/main/java/edu/fiuba/algo3/interfazGrafica/pregunta/MultipleChoicePanel.java
+++ src/main/java/edu/fiuba/algo3/interfazGrafica/pregunta/MultipleChoicePanel.java
@@ -1,21 +1,18 @@
 package edu.fiuba.algo3.interfazGrafica.pregunta;
 
 import edu.fiuba.algo3.modelo.pregunta.Opcion;
-import javafx.scene.layout.VBox;
 
 import javax.swing.*;
-import java.awt.*;
-import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class MultipleChoicePanel extends JPanel implements JPanelPregunta{
 
-    private ButtonGroup opcionesButtonGroup;
+    private LinkedList<JCheckBox> opcionesEnPantalla;
 
     public MultipleChoicePanel(List<Opcion> opciones){
-
-        opcionesButtonGroup = new ButtonGroup();
+        opcionesEnPantalla = new LinkedList<JCheckBox>();
         opciones.stream().forEach(opcion -> agregarOpcion(opcion));
     }
 
@@ -23,19 +20,19 @@ public class MultipleChoicePanel extends JPanel implements JPanelPregunta{
         JCheckBox opcionCheckBox = new JCheckBox();
         opcionCheckBox.setText(opcion.obtenerTexto());
         add(opcionCheckBox);
-        opcionesButtonGroup.add(opcionCheckBox);
+        opcionesEnPantalla.add(opcionCheckBox);
 
     }
 
     @Override
     public List<String> obtenerOpcionesSeleccionadas() {
-        List<AbstractButton> botonesSeleccionados = obtenerBotonesSeleccionados(Collections.list(opcionesButtonGroup.getElements()));
+        List<JCheckBox> botonesSeleccionados = obtenerBotonesSeleccionados(opcionesEnPantalla);
         return botonesSeleccionados.stream()
                 .map(button -> button.getText())
                 .collect(Collectors.toList());
     }
 
-    private List<AbstractButton> obtenerBotonesSeleccionados(List<AbstractButton> botones){
+    private List<JCheckBox> obtenerBotonesSeleccionados(List<JCheckBox> botones){
         return botones.stream().filter(checkBox -> checkBox.isSelected()).collect(Collectors.toList());
     }
 
